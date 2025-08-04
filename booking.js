742997bd07f3409eed4edcf6bb0cb2601f520a3d
Riya <riyakhatawkar@gmail.com>
2025-08-04 15:19:44 +0530

    new change

--- booking.js
+++ booking.js
@@ -1,9 +1,8 @@
-
-  function attachDateFormat(id) {
+function attachDateFormat(id) {
     const input = document.getElementById(id);
     input.addEventListener("input", () => {
       input.value = input.value
-        .replace(/[^0-9-]/g, "") // allow only numbers and dash
+        .replace(/[^0-9-]/g, "") 
         .replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3"); // format yyyy-mm-dd
     });
   }
@@ -11,9 +10,21 @@
   attachDateFormat("checkin");
   attachDateFormat("checkout");
 
-  fetch('http://localhost:8080/booking')
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById('output').innerText = data.message;
-  })
-  .catch(err => console.error(err));
+  function sendBookingRequest() {
+    const checkin = document.getElementById("checkin").value;
+    const checkout = document.getElementById("checkout").value;
+
+    fetch("http://localhost:8080/booking", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ checkin, checkout })
+    })
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("output").innerText = data.message;
+    })
+    .catch(err => console.error(err));
+  }
+ document.getElementById("submitBtn").addEventListener("click", sendBookingRequest);
